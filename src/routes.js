9642d0c3dc8b493dfbc89db1f6d365f5ceb057dd
libeanim <libeanim@users.noreply.github.com>
2017-10-08 15:16:01 +0800

    Add routes for marko-path-router

---
+++ src/routes.js
@@ -0,0 +1,5 @@
+module.exports = [
+  { path: '/', component: require('./components/index.marko') },
+  { path: '/about', component: require('./components/about.marko') },
+  { path: '/404', component: require('./components/not-found.marko') },
+]
